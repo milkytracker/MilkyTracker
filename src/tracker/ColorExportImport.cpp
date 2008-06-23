820be614560d08fb03bdbecdc97a07fec291551c
Peter Barth (pailes) <peter@mx.nxbone.net>
2008-06-23 17:14:26 +0000

    ADDED: Color table import/export

---
+++ src/tracker/ColorExportImport.cpp
@@ -0,0 +1,74 @@
+/*
+ *  tracker/ColorExportImport.cpp
+ *
+ *  Copyright 2008 Peter Barth
+ *
+ *  This file is part of Milkytracker.
+ *
+ *  Milkytracker is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Milkytracker is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Milkytracker.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/*
+ *  ColorExportImport.cpp
+ *  MilkyTracker
+ *
+ *  Created by Peter Barth on Sun Mar 13 2005.
+ *
+ */
+
+#include "ColorExportImport.h"
+#include "TrackerSettingsDatabase.h"
+#include "XMFile.h"
+
+bool ColorExportImport::importColorPalette(TColorPalette& palette)
+{
+	TrackerSettingsDatabase colorDataBase;
+
+	XMFile f(fileName);
+
+	if (!f.isOpen())
+	{
+		// deal with error
+		return false;
+	}
+	
+	colorDataBase.serialize(f);
+	
+	PPDictionaryKey* key = colorDataBase.restore("ACTIVECOLORS");
+	
+	if (key)
+	{
+		palette = ColorPaletteContainer::decodePalette(key->getStringValue());			
+	}	
+	
+	return true;
+}
+
+bool ColorExportImport::exportColorPalette(const TColorPalette& palette)
+{
+	TrackerSettingsDatabase colorDataBase;
+	colorDataBase.store("ACTIVECOLORS", ColorPaletteContainer::encodePalette(palette));
+		
+	XMFile f(fileName, true);
+	if (!f.isOpenForWriting())
+	{
+		// deal with error
+		return false;
+	}
+		
+	colorDataBase.serialize(f);
+	
+	return true;
+}
---
+++ src/tracker/ColorExportImport.h
@@ -0,0 +1,52 @@
+/*
+ *  tracker/ColorExportImport.h
+ *
+ *  Copyright 2008 Peter Barth
+ *
+ *  This file is part of Milkytracker.
+ *
+ *  Milkytracker is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Milkytracker is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Milkytracker.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/*
+ *  ColorExportImport.h
+ *  MilkyTracker
+ *
+ *  Created by Peter Barth on Sun Mar 13 2005.
+ *
+ */
+ 
+#ifndef __COLOREXPORTIMPORT_H__
+#define __COLOREXPORTIMPORT_H__
+
+#include "BasicTypes.h"
+#include "ColorPaletteContainer.h"
+
+class ColorExportImport
+{
+private:
+	PPSystemString fileName;
+
+public:
+	ColorExportImport(const PPSystemString& fileName) :
+		fileName(fileName)
+	{
+	}
+	
+	bool importColorPalette(TColorPalette& palette);
+	bool exportColorPalette(const TColorPalette& palette);	
+};
+
+#endif
