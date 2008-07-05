2394f155945c85e6a85b5bbb2b96d2ff0da56286
Peter Barth (pailes) <peter@mx.nxbone.net>
2008-07-05 13:19:32 +0000

    ADDED: gzip decompression

---
+++ src/compression/DecompressorGZIP.cpp
@@ -0,0 +1,97 @@
+/*
+ *  tracker/DecompressorGZIP.cpp
+ *
+ *  Copyright 2008 Peter Barth
+ *
+ *  This file is part of Milkytracker.
+ *
+ *  Milkytracker is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Milkytracker is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Milkytracker.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+ 
+/*
+ *  DecompressorGZIP.cpp
+ *  milkytracker_universal
+ *
+ *  Created by Peter Barth on 22.06.08.
+ *
+ */
+
+#include "DecompressorGZIP.h"
+#include "XMFile.h"
+#include "LittleEndian.h"
+#include "zlib.h"
+
+// -- GZIP --------------------------------------------------------------------
+DecompressorGZIP::DecompressorGZIP(const PPSystemString& fileName) :
+	DecompressorBase(fileName)
+{
+}
+
+bool DecompressorGZIP::identify(XMFile& f)
+{
+	f.seek(0);
+	mp_dword id = f.readDword();
+	
+	// GZIP ID
+	return (id == 0x08088B1F);
+}
+	
+	
+bool DecompressorGZIP::decompress(const PPSystemString& outFileName)
+{
+    gzFile *gz_input_file = NULL;
+	int len = 0;
+	pp_uint8 *buf;
+	
+	if ((gz_input_file = (void **)gzopen (fileName.getStrBuffer(), "r")) == NULL)
+		return false;
+	
+	if ((buf = new pp_uint8[0x10000]) == NULL)
+		return false;
+	
+	XMFile fOut(outFileName, true);
+	
+    while (true) 
+	{
+        len = gzread (gz_input_file, buf, 0x10000);
+		
+        if (len < 0) 
+		{
+	        delete[] buf;
+            return false;
+        }
+        
+		if (len == 0) break;
+		
+		fOut.write(buf, 1, len);
+    }
+	
+    if (gzclose (gz_input_file) != Z_OK)
+	{
+	    delete[] buf;
+        return false;
+    }
+	
+	delete[] buf;
+	
+	return true;
+}
+
+DecompressorBase* DecompressorGZIP::clone()
+{
+	return new DecompressorGZIP(fileName);
+}	
+
+static Decompressor::RegisterDecompressor<DecompressorGZIP> registerDecompressor;
---
+++ src/compression/DecompressorGZIP.h
@@ -0,0 +1,53 @@
+/*
+ *  tracker/DecompressorGZIP.h
+ *
+ *  Copyright 2008 Peter Barth
+ *
+ *  This file is part of Milkytracker.
+ *
+ *  Milkytracker is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Milkytracker is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Milkytracker.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/*
+ *  DecompressorGZIP.h
+ *  milkytracker_universal
+ *
+ *  Created by Peter Barth on 22.06.08.
+ *
+ */
+
+#ifndef __DECOMPRESSOR_GZIP_H__
+#define __DECOMPRESSOR_GZIP_H__
+
+#include "Decompressor.h"
+
+/*****************************************************************************
+ * gzip decompression
+ *****************************************************************************/
+class DecompressorGZIP : public DecompressorBase
+{
+public:
+	DecompressorGZIP(const PPSystemString& fileName);
+
+	virtual bool identify(XMFile& f);
+	
+	virtual bool decompress(const PPSystemString& outFileName);
+	
+	virtual DecompressorBase* clone();
+};
+
+
+#endif
+
