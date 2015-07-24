7c98cc8300acdf5a7e4dc3ac577b14db0bcb6c7d
Christopher O'Neill <code@chrisoneill.co.uk>
2015-07-24 10:07:29 +0100

    Fix XM loader crash when loading <4 byte samples
    This fixes this bug:
    
    http://modarchive.org/forums/index.php?topic=3608.0
    
    "Crash when loading .s3m"
    
    MilkyTracker imports the .s3m module then exports it out as an XM file
    before re-loading it.  Instrument 10 contained a 2 byte sample which
    resulted in MilkyTracker crashing while loading the XM file.

--- src/milkyplay/XModule.cpp
+++ src/milkyplay/XModule.cpp
@@ -159,8 +159,8 @@ void TXMSample::restoreOriginalState()
 void TXMSample::postProcessSamples()
 {
 	mp_ubyte buffer[8];
-	mp_uint32 loopend = loopstart + looplen;
-	mp_uint32 samplen = this->samplen;
+	mp_sint32 loopend = loopstart + looplen;
+	mp_sint32 samplen = this->samplen;
 
 	// this is in fact not good
 	if (loopend > samplen)
@@ -1486,7 +1486,7 @@ bool XModule::loadSample(XMFileBase& f,void* buffer,mp_uint32 size,mp_uint32 len
 	else
 	{
 		memset(buffer, 0, size);
-		f.read(buffer,1,size);
+		f.read(buffer,1,length);
 	}
 
 	// 16 bit sample 
