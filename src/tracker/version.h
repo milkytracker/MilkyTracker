077003ccb5d5fa77e530ca50efe1e72df7349c5e
Christopher O'Neill <code@chrisoneill.co.uk>
2017-03-11 18:42:27 +0000

    Bump version to 1.0.0, update .nfo

--- src/tracker/version.h
+++
@@ -1,4 +0,0 @@
-const int MILKYTRACKER_VERSION = 0x009087;  // 0xXXYYZZ = XX.YY.ZZ
-// Version string restricted to 20 chars for XM export!
-const char MILKYTRACKER_VERSION_STRING[] = "MilkyTracker 0.90.87";
-//                                          012345678901234567890
