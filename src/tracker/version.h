4cf0f2611e55c8a3eb847c914f44f541c53ac6e6
Marlon Beijer <marlon@eevul.org>
2017-09-17 00:23:01 +0200

    Bumped version to current!

--- src/tracker/version.h
+++ src/tracker/version.h
@@ -1,5 +1,5 @@
 // 0xXYYZZ = X.YY.ZZ
-const int MILKYTRACKER_VERSION = 0x10000;
+const int MILKYTRACKER_VERSION = 0x10100;
 // Version string restricted to 20 chars for XM export!
-const char MILKYTRACKER_VERSION_STRING[] = "MilkyTracker 1.0.0";
+const char MILKYTRACKER_VERSION_STRING[] = "MilkyTracker 1.01.00";
 //                                          012345678901234567890
