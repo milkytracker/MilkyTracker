b6ad71029c4bc7893f2264100bc9b96282ca3651
Johannes Schultz <sagamusix@openmpt.org>
2017-10-06 16:00:56 +0200

    Fix stack overflow loading XM instruments with more than 16 samples
    See issue #35.
    
    Fixes id_000014,sig_06,sync_xmp2-crash-import,src_000465

--- src/milkyplay/LoaderXM.cpp
+++ src/milkyplay/LoaderXM.cpp
@@ -63,7 +63,7 @@ const char* LoaderXM::identifyModule(const mp_ubyte* buffer)
 mp_sint32 LoaderXM::load(XMFileBase& f, XModule* module)
 {
 	mp_ubyte insData[230];		
-	mp_sint32 smpReloc[16];
+	mp_sint32 smpReloc[96];
 	mp_ubyte nbu[96];
 	mp_uint32 fileSize = 0;
 			
@@ -141,7 +141,11 @@ mp_sint32 LoaderXM::load(XMFileBase& f, XModule* module)
 			f.readDwords(&instr[y].size,1);
 			f.read(&instr[y].name,1,22);		
 			f.read(&instr[y].type,1,1);
-			f.readWords(&instr[y].samp,1);
+			mp_uword numSamples = 0;
+			f.readWords(&numSamples,1);
+			if(numSamples > 96)
+				return MP_LOADER_FAILED;
+			instr[y].samp = numSamples;
 
 			if (instr[y].size == 29)
 			{
