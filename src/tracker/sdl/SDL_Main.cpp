857d3d68cb3bc2cf3da50bc9daff9325d05179a4
Christopher O'Neill <code@chrisoneill.co.uk>
2018-02-27 10:21:34 +0000

    Fix #151: clang6.0.0 (freeBSD) build failure
    Add explicit cast for narrowing conversion to hopefully fix the build failure.

--- src/tracker/sdl/SDL_Main.cpp
+++ src/tracker/sdl/SDL_Main.cpp
@@ -609,7 +609,7 @@ void translateKeyDownEvent(const SDL_Event& event)
 	printf ("DEBUG: Key pressed: VK: %d, SC: %d, Scancode: %d\n", toVK(keysym), toSC(keysym), keysym.sym);
 #endif
 
-	pp_uint16 chr[3] = {toVK(keysym), toSC(keysym), keysym.sym};
+	pp_uint16 chr[3] = {toVK(keysym), toSC(keysym), static_cast<pp_uint16> (keysym.sym)};
 
 	PPEvent myEvent(eKeyDown, &chr, sizeof(chr));
 	RaiseEventSerialized(&myEvent);
@@ -621,7 +621,7 @@ void translateKeyUpEvent(const SDL_Event& event)
 
 	preTranslateKey(keysym);
 
-	pp_uint16 chr[3] = {toVK(keysym), toSC(keysym), keysym.sym};
+	pp_uint16 chr[3] = {toVK(keysym), toSC(keysym), static_cast<pp_uint16> (keysym.sym)};
 
 	PPEvent myEvent(eKeyUp, &chr, sizeof(chr));
 	RaiseEventSerialized(&myEvent);
