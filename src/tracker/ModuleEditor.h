6833d61c75dc48b3aa23a07a84a759cd1a722f6f
Leon <info@leon.vankammen.eu>
2021-07-06 15:59:46 +0200

    easily 'sample' current pattern into current sample

--- src/tracker/ModuleEditor.h
+++ src/tracker/ModuleEditor.h
@@ -34,6 +34,7 @@ class SampleEditor;
 class EnvelopeEditor;
 class ModuleServices;
 class PlayerCriticalSection;
+class Tracker;
 
 class ModuleEditor
 {
@@ -385,6 +386,7 @@ public:
 	static PPSystemString getTempFilename();
 	
 	friend class ChangesListener;
+	friend class Tracker;
 };
 
 #endif
