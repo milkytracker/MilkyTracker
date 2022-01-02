cc63ada14d58cb9fd468106755b1345889bf3d91
Marlon Beijer <marlon@eevul.org>
2022-01-02 19:39:58 +0100

    Update prep script

--- platforms/m68k-amigaos/prep.sh
+++ platforms/m68k-amigaos/prep.sh
@@ -14,33 +14,34 @@ export CURPATH="${PWD}"
 export SUBMODULES="${CURPATH}/src/submodules"
 
 # ZLIB
-rm -rf ${SUBMODULES}/zlib/build
-mkdir -p ${SUBMODULES}/zlib/build
-cd ${SUBMODULES}/zlib/build
+rm -rf "${SUBMODULES}"/zlib/build
+mkdir -p "${SUBMODULES}"/zlib/build
+cd "${SUBMODULES}"/zlib/build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${SYSROOT} -DM68K_CPU=68040 -DM68K_FPU=hard -DM68K_COMMON="${M68K_COMMON} -O3 -fno-exceptions -w -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14"
 cmake --build . --config Release --target install -- -j$(getconf _NPROCESSORS_ONLN)
-cd ${SUBMODULES}
+cd "${SUBMODULES}"
 
 # SDL1.2
 if [ ! -d "${SUBMODULES}/SDL" ]; then
-	git clone https://github.com/AmigaPorts/SDL.git ${SUBMODULES}/SDL
+	git clone https://github.com/AmigaPorts/SDL.git "${SUBMODULES}"/SDL
 fi
-cd ${SUBMODULES}/SDL
+cd "${SUBMODULES}"/SDL
 git checkout SDL-1.2-AmigaOS3
 git pull
-rm -rf ${SUBMODULES}/SDL/build
-mkdir -p ${SUBMODULES}/SDL/build
-cd ${SUBMODULES}/SDL/build
+rm -rf "${SUBMODULES}"/SDL/build
+mkdir -p "${SUBMODULES}"/SDL/build
+cd "${SUBMODULES}"/SDL/build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${SYSROOT} -DM68K_CPU=68040 -DM68K_FPU=hard -DM68K_COMMON="${M68K_COMMON} -O3 -fno-exceptions -w -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14"
 cmake --build . --config Release --target install -- -j$(getconf _NPROCESSORS_ONLN)
-cd ${SUBMODULES}
+cd "${SUBMODULES}"
 
 # Zziplib
-#cd ${SUBMODULES}/zziplib
-#CFLAGS="${M68K_CFLAGS}" CXXFLAGS="${M68K_CXXFLAGS}" ./configure --disable-sdltest --disable-shared --enable-static --host=${TARGET} --with-sysroot=${SYSROOT} --with-zlib=${SYSROOT}
-#make -j$(getconf _NPROCESSORS_ONLN)
-#make install
-#cd ${SUBMODULES}
+#rm -rf "${SUBMODULES}"/zziplib/build
+#mkdir -p "${SUBMODULES}"/zziplib/build
+#cd "${SUBMODULES}"/zziplib/build
+#cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${SYSROOT} -DM68K_CPU=68040 -DM68K_FPU=hard -DM68K_COMMON="${M68K_COMMON} -O2 -fno-exceptions -w -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14"
+#cmake --build . --config Release --target install -- -j$(getconf _NPROCESSORS_ONLN)
+#cd "${SUBMODULES}"
 
 # lhasa
 #cd ${SUBMODULES}/lhasa
