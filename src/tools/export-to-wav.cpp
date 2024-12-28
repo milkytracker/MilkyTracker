c4ed56345e925b72e5fe5e1b437a2cb2d5911e52
Tom Rees-Herdman <tom@techanvil.co.uk>
2024-12-28 01:42:03 +0000

    Use ModuleServices::exportToWAV instead of PlayerGeneric::exportToWAV.

--- src/tools/export-to-wav.cpp
+++ src/tools/export-to-wav.cpp
@@ -1,5 +1,7 @@
 #include <milkyplay/PlayerGeneric.h>
 #include <milkyplay/XModule.h>
+#include <tracker/ModuleServices.h>
+#include <ppui/osinterface/PPSystem.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -26,7 +28,7 @@ int main(int argc, char* argv[]) {
     }
 
     const char* inputFile = argv[1];
-    const char* outputFile = argv[2];
+    PPSystemString outputFile = argv[2];
     bool debug_enabled = false;
 
     // Create module and load file
@@ -43,12 +45,20 @@ int main(int argc, char* argv[]) {
     DEBUG_LOG("Module loaded successfully. Orders: %d, Channels: %d\n", 
             module->header.ordnum, module->header.channum);
 
-    // Create player with default 44.1kHz sample rate
-    PlayerGeneric player(44100);
+    // Create ModuleServices instance
+    ModuleServices services(*module);
 
-    // Default parameters
-    int startOrder = 0;
-    int endOrder = -1;
+    // Setup export parameters with defaults
+    ModuleServices::WAVWriterParameters params;
+    params.sampleRate = 44100;
+    params.resamplerType = 0;
+    params.rampin = false;
+    params.playMode = 0;
+    params.mixerShift = 0;
+    params.mixerVolume = 256;
+    params.fromOrder = 0;
+    params.toOrder = module->header.ordnum - 1;
+    params.multiTrack = false;
 
     // Parse command line options
     for (int i = 3; i < argc; i++) {
@@ -56,86 +66,63 @@ int main(int argc, char* argv[]) {
             debug_enabled = true;
         }
         else if (strcmp(argv[i], "--samplerate") == 0 && i + 1 < argc) {
-            int freq = atoi(argv[++i]);
-
-            DEBUG_LOG("Setting sample rate: %d Hz\n", freq);
-
-            player.adjustFrequency(freq);
+            params.sampleRate = atoi(argv[++i]);
+            DEBUG_LOG("Setting sample rate: %d Hz\n", params.sampleRate);
         }
         else if (strcmp(argv[i], "--ramping") == 0 && i + 1 < argc) {
             int ramp = atoi(argv[++i]);
-
+            params.rampin = (ramp == 2);
+            params.resamplerType = (ramp > 0) ? 1 : 0;
             DEBUG_LOG("Setting ramping: %d\n", ramp);
-
-            player.setResamplerType(ramp > 0, ramp == 2);
         }
         else if (strcmp(argv[i], "--resampler") == 0 && i + 1 < argc) {
-            int type = atoi(argv[++i]);
-
-            DEBUG_LOG("Setting resampler type: %d\n", type);
-
-            player.setResamplerType((ChannelMixer::ResamplerTypes)(type));
+            params.resamplerType = atoi(argv[++i]);
+            DEBUG_LOG("Setting resampler type: %d\n", params.resamplerType);
         }
         else if (strcmp(argv[i], "--mixershift") == 0 && i + 1 < argc) {
-            int shift = atoi(argv[++i]);
-
-            DEBUG_LOG("Setting mixer shift: %d\n", shift);
-
-            player.setSampleShift(shift);
+            params.mixerShift = atoi(argv[++i]);
+            DEBUG_LOG("Setting mixer shift: %d\n", params.mixerShift);
         }
         else if (strcmp(argv[i], "--mixervolume") == 0 && i + 1 < argc) {
-            int vol = atoi(argv[++i]);
-
-            DEBUG_LOG("Setting mixer volume: %d\n", vol);
-
-            player.setMasterVolume(vol);
+            params.mixerVolume = atoi(argv[++i]);
+            DEBUG_LOG("Setting mixer volume: %d\n", params.mixerVolume);
         }
         else if (strcmp(argv[i], "--from") == 0 && i + 1 < argc) {
-            startOrder = atoi(argv[++i]);
-
-            DEBUG_LOG("Setting start order: %d\n", startOrder);
+            params.fromOrder = atoi(argv[++i]);
+            DEBUG_LOG("Setting start order: %d\n", params.fromOrder);
         }
         else if (strcmp(argv[i], "--to") == 0 && i + 1 < argc) {
-            endOrder = atoi(argv[++i]);
-            
-            DEBUG_LOG("Setting end order: %d\n", endOrder);
+            params.toOrder = atoi(argv[++i]);
+            DEBUG_LOG("Setting end order: %d\n", params.toOrder);
         }
     }
 
     // Validate order range
-    if (endOrder == -1) {
-        endOrder = module->header.ordnum - 1;
-        DEBUG_LOG("Using default end order: %d\n", endOrder);
-    }
-    if (startOrder < 0 || startOrder >= module->header.ordnum) {
-        fprintf(stderr, "Start order %d out of range (0-%d)\n", startOrder, module->header.ordnum - 1);
+    if (params.fromOrder < 0 || params.fromOrder >= module->header.ordnum) {
+        fprintf(stderr, "Start order %d out of range (0-%d)\n", params.fromOrder, module->header.ordnum - 1);
         delete module;
         return 1;
     }
-    if (endOrder < startOrder || endOrder >= module->header.ordnum) {
-        fprintf(stderr, "End order %d out of range (%d-%d)\n", endOrder, startOrder, module->header.ordnum - 1);
+    if (params.toOrder < params.fromOrder || params.toOrder >= module->header.ordnum) {
+        fprintf(stderr, "End order %d out of range (%d-%d)\n", params.toOrder, params.fromOrder, module->header.ordnum - 1);
         delete module;
         return 1;
     }
 
-    DEBUG_LOG("Exporting to WAV file: %s\n", outputFile);
-    DEBUG_LOG("Orders to export: %d through %d\n", startOrder, endOrder);
-
-    // Set buffer size for better performance
-    DEBUG_LOG("Setting buffer size to 1024 samples\n");
-    player.setBufferSize(1024);
+    DEBUG_LOG("Exporting to WAV file: %s\n", (const char*)outputFile);
+    DEBUG_LOG("Orders to export: %d through %d\n", params.fromOrder, params.toOrder);
 
     // Export to WAV with proper parameters
     DEBUG_LOG("Starting export...\n");
     
-    int numWrittenSamples = player.exportToWAV(outputFile, module, startOrder, endOrder);
+    int numWrittenSamples = services.exportToWAV(outputFile, params);
     if (numWrittenSamples == MP_DEVICE_ERROR) {
-        fprintf(stderr, "Failed to export WAV file: %s\n", outputFile);
+        fprintf(stderr, "Failed to export WAV file: %s\n", (const char*)outputFile);
         delete module;
         return 1;
     }
 
     delete module;
-    printf("Successfully exported to WAV: %s. %d samples written\n", outputFile, numWrittenSamples);
+    printf("Successfully exported to WAV: %s. %d samples written\n", (const char*)outputFile, numWrittenSamples);
     return 0;
 } 
