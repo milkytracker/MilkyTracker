5e19d9f96d8442951b8ca49b0763ea0368a4f713
Tom Rees-Herdman <tom@techanvil.co.uk>
2025-01-25 03:23:18 +0000

    Update input file argument description for module file

--- src/tracker/cocoa/main.mm
+++ src/tracker/cocoa/main.mm
@@ -65,7 +65,7 @@ void QueryKeyModifiers() { }
 int main(int argc, const char * argv[])
 {
 	static CLIParser parser(argc, argv);
-	parser.addPositionalArg("input", "Input file", false);
+	parser.addPositionalArg("input", "Input module file (.xm)", false);
 	parser.addOption("--headless", false, "Run in headless mode");
 
 	auto exporter = WAVExporter::createFromParser(parser);
